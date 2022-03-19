d9a2867f88991442e89c7d9919812b56befbc6bd
dontroll17 <dontroll@yandex.ru>
2022-03-19 14:05:18 +0300

    init: init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#Ip info
+## ip, org, country, city, zip code, latitude and longitude
---
+++ index.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const get_ip = async (ip = '127.0.0.1') => {
+    try{
+        const response = await axios.get(`http://ip-api.com/json/${ip}`);
+        const data = {
+            '#IP': response.data.query,
+            "#Int prov": response.data.isp,
+            "#Org": response.data.org,
+            "#Country": response.data.country,
+            "#Region name": response.data.regionName,
+            "#City": response.data.city,
+            "#ZIP code": response.data.zip,
+            "#Lat": response.data.lat,
+            "#Lon": response.data.lon
+        }
+        const ctx = Object.entries(data).join('\n').replace(/,/g, ": ");
+        console.log(ctx)
+    }catch(e) {
+        console.error(e);
+    }
+}
+
+rl.question('Enter ip: ', (ip) => {
+    get_ip(ip).then(x => {
+        return x;
+    });
+    rl.close();
+});
+
+
